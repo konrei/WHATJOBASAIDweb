9a2c66214adecebb0016df089c6f5bf20498dc57
Samet Cömert <cokciddisin@gmail.com>
2018-12-25 23:14:06 +0300

    wow

--- node1/bot.js
+++ node1/bot.js
@@ -1,1 +1,9 @@
 console.log("sup")
+
+var Twit = require('twit'); //@import
+var T = new Twit({
+  consumer_key:         '...',
+  consumer_secret:      '...',
+  access_token:         '...',
+  access_token_secret:  '...',  
+})
