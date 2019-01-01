ce11a5f2a41999d13f78a234ac593931b6c014c0
Samet Cömert <cokciddisin@gmail.com>
2019-01-01 17:01:44 +0300

    haha

--- node1/bot.js
+++ node1/bot.js
@@ -1,3 +1,15 @@
 var Twit = require('twit'); //@import
-var Config = require('./config');
-var T = new Twit(Config);
+var Config = require('./config'); //this one too
+var T = new Twit(Config); //tokens and keys
+
+var params = { q: 'joba', count: 10 };
+
+function gotData(err, data, response) {
+  var tweets = data.statuses;
+  for(var i = 0; i < tweets.length; i++)
+  {
+  	console.log(tweets[i].text);
+  }
+};
+
+T.get('search/tweets', params, gotData);
--- node1/config.js
+++ node1/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  consumer_key:         '...',
-  consumer_secret:      '...',
-  access_token:         '...',
-  access_token_secret:  '...',
+  consumer_key:         'n9iCsG6yO0JWopW0krEx3Lld0',
+  consumer_secret:      '1Ije34Iv3Bi0FmhqeEQbagl0eH6PHhFaM1cVPFwiUlMylnfqKM',
+  access_token:         '827391774486261760-eDnBUB9ofIGy9h5sMoS8Uk3zfldehDD',
+  access_token_secret:  'pwB8ndLRH0TLthGIUU081x9v8rEIijEazNaaqQv6DhDER',
 }
