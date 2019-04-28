1777113f487614de9157f15d16687391f501b552
Samet Cömert <cokciddisin@gmail.com>
2019-04-28 13:23:52 +0300

    no message

--- src/config.js
+++ src/config.js
@@ -1,6 +1,4 @@
-module.exports = {
-  consumer_key:         'n9iCsG6yO0JWopW0krEx3Lld0',
-  consumer_secret:      '1Ije34Iv3Bi0FmhqeEQbagl0eH6PHhFaM1cVPFwiUlMylnfqKM',
-  access_token:         '827391774486261760-eDnBUB9ofIGy9h5sMoS8Uk3zfldehDD',
-  access_token_secret:  'pwB8ndLRH0TLthGIUU081x9v8rEIijEazNaaqQv6DhDER',
-}
+export const consumer_key = 'n9iCsG6yO0JWopW0krEx3Lld0';
+export const consumer_secret = '1Ije34Iv3Bi0FmhqeEQbagl0eH6PHhFaM1cVPFwiUlMylnfqKM';
+export const access_token = '827391774486261760-eDnBUB9ofIGy9h5sMoS8Uk3zfldehDD';
+export const access_token_secret = 'pwB8ndLRH0TLthGIUU081x9v8rEIijEazNaaqQv6DhDER';
